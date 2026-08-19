e71e0b4467c788d68d6e2fa218f7921a6320bc5d
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-08-19 13:04:59 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,11 +1,11 @@
 import pymongo
 from pyrogram import enums
-from info import DATABASE_URI, DATABASE_NAME
+from info import OTHERDB_URI, DATABASE_NAME
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI)
+myclient = pymongo.MongoClient(OTHERDB_URI)
 mydb = myclient[DATABASE_NAME]
 
 
