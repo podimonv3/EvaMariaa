dbde4e3166cb8bbdbfbf3eeb9268fb2a49ffa04d
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-08-19 13:12:43 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,6 +1,6 @@
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
+from info import DATABASE_NAME, OTHERDB_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
 
 class Database:
     
@@ -217,4 +217,4 @@ class Database:
         await self.req_two.delete_many({})
         
         
-db = Database(DATABASE_URI, DATABASE_NAME)
+db = Database(OTHERDB_URI, DATABASE_NAME)
