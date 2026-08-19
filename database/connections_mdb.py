8c9902448bd42e87e35f896f6f1a28067033dfb2
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-08-19 13:04:10 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,12 +1,12 @@
 import pymongo
 
-from info import DATABASE_URI, DATABASE_NAME
+from info import OTHERDB_URI, DATABASE_NAME
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI)
+myclient = pymongo.MongoClient(OTHERDB_URI)
 mydb = myclient[DATABASE_NAME]
 mycol = mydb['CONNECTION']   
 
