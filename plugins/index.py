b8ee9f40b919a8292aa72c4aca86633ebecf1a95
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-09-02 16:03:34 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -103,7 +103,8 @@ async def send_for_index(bot, message):
         last_msg_id = int(match.group(5))
         if chat_id.isnumeric():
             chat_id  = int(("-100" + chat_id))
-    elif message.forward_from_chat.type == enums.ChatType.CHANNEL:
+        # പഴയ വരിക്ക് പകരം ഈ വരി ഉപയോഗിക്കുക:
+    elif message.forward_from_chat and message.forward_from_chat.type == enums.ChatType.CHANNEL:
         last_msg_id = message.forward_from_message_id
         chat_id = message.forward_from_chat.username or message.forward_from_chat.id
     else:
